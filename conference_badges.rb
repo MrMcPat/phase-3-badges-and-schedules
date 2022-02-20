f335560215402d0ccce47c81d921ead97915e5ce
Patrick Liang <pl204511194@yahoo.com>
2022-02-20 11:21:10 -0500

    commit tree 51f1537ee67a5660b12758a7ba99091c34365e16

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms array
+    num = 1
+    array.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer array
+    batch_badge_creator(array).each do |name|
+        puts name
+    end
+    assign_rooms(array).each do |name|
+        puts name
+    end
+end
